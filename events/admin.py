5803bf4aa8d5fb73f39a0aa2476ba8819f36bdb2
emanuel <emanueljuniortec@gmail.com>
2018-09-11 21:16:32 -0300

    add coments

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Comments
+
 
 admin.site.register(Event)
+admin.site.register(Comments)
 
 
 # Register your models here.
--- events/models.py
+++ events/models.py
@@ -15,5 +15,14 @@ class Event(models.Model):
     event = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=priorities_list)
 
+
     def __str__(self):
         return self.event
+
+class Comments(models.Model):
+    nome = models.CharField(max_length=100)
+    date = models.DateField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.nome
