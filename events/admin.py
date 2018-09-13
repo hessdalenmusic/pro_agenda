7e7abf6dd956c5678567f354ac021ee3d8904498
emanuel <emanueljuniortec@gmail.com>
2018-09-13 19:07:14 -0300

    melhorando o admin de events

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Event, Comments
 
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("date","event","priority")
+    list_display_links = ("event",)
+    list_filter = ("date","priority")
+    list_editable = ("priority",)
+    search_fields = ("event","date")
 
-admin.site.register(Event)
+
+
 admin.site.register(Comments)
 
 
